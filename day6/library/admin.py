434a07823db4d4462fff7bde65a40341aaec744f
vedantsalvi10 <vedantsalvi10@gmail.com>
2025-03-19 12:21:37 +0530

    completing day6

--- day6/library/admin.py
+++ day6/library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from library.models import Book,Author,Genre
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
